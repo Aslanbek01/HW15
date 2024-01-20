0cc9b1e35abb28924a0250d3a6cf5e99c437299e
Aslanbek01 <99314048+Aslanbek01@users.noreply.github.com>
2024-01-21 02:41:37 +0600

    HW22

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
 from docx import Document
-from docx.shared import Pt
-from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 
-doc = Document()
-doc.add_paragraph('Hello Python', 'Title')
-doc.save(r"C:\Users\Aslan\Desktop\HW15.docx")
+def create_word_document(text):
+    doc = Document()
+    doc.add_paragraph(text)
 
-doc = Document(r"C:\Users\Aslan\Desktop\HW15.docx")
-for para in doc.paragraphs:
-    for run in para.runs:
-        if run.bold:
-            print(run.text)
+    filename = "output.docx"
+    doc.save(filename)
+    print(f"Файл '{filename}' успешно создан.")
 
-new_doc = Document()
-para = new_doc.add_paragraph('Новый абзац текста')
-run = para.runs[0]
-run.font.name = 'Arial'
-run.font.size = Pt(14)
-para.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
-new_doc.save(r"C:\Users\Aslan\Desktop\HW15.docx")
-print(run.bold)
-print(para.alignment)
+def main():
+    user_input = input("Aslanbek Smagulov HW ")
+    create_word_document(user_input)
+
+if __name__ == "__main__":
+    main()
